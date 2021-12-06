4909a134f70cecefc12899f772a4f5caa18ce4f2
Christian <cri-9999@live.it>
2021-12-06 01:00:43 +0100

    updated .gitignore file, build folder no longer gets committed

--- build/Utils/isBoolean.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isBoolean = void 0;
-var isBoolean = function (value) {
-    return value === "true" || value === "false";
-};
-exports.isBoolean = isBoolean;
--- build/Utils/misc.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isVibrationSensor = void 0;
-var isVibrationSensor = function (type) {
-    return type === "TimeDomainDataInfo" || type === "RMSSpeedStatus" || type === "AccPeakStatus" || type === "FreqData";
-};
-exports.isVibrationSensor = isVibrationSensor;
