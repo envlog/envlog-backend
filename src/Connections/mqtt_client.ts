f46d8dbd360fac8c01c387586a880f103d01abc9
ChrisIT9 <cri-9999@live.it>
2021-12-09 18:17:12 +0100

    changed mqtt broker

--- src/Connections/mqtt_client.ts
+++ src/Connections/mqtt_client.ts
@@ -2,7 +2,10 @@ import MQTT from 'async-mqtt';
 const topics = ['Temperature/+', 'RMSSpeedStatus/+', 'AccPeakStatus/+', 'FreqData/+', 'Battery/+', 'TimeDomainDataInfo/+', 
                 'Pressure/+', 'Infrared/+', 'Brightness/+', 'Anemometer/+'];
 
-const mqttClient = MQTT.connect(process.env.MQTT_BROKER);
+const mqttClient = MQTT.connect(`${process.env.MQTT_BROKER}:${process.env.MQTT_PORT}`, {
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD
+});
 
 mqttClient.on('connect', async () => {
     
