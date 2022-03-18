e1f4c3e63040a05a1e5272cab8f93de0ae920b4d
nogards95TG <page271295@gmail.com>
2022-03-18 11:11:55 +0100

    feat: swagger init

--- src/app.ts
+++ src/app.ts
@@ -14,12 +14,19 @@ import sensorsRouter from './Routes/Sensors';
 import { staticFolder } from './Config/path';
 import cors from 'cors';
 import sensorsDataRouter from './Routes/SensorsDataHistory';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
+
+const swaggerDocument = YAML.load('./public/openapi.yml');
+
 
 app.use(cors({ credentials: true, origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/auth/login', loginRouter);
 app.use('/auth/register', registerRouter);
 app.use('/auth/logout', logoutRouter);
