ee0d0a11904d7c7e27115a5fdaa7f06b69a1effc
nogards95TG <page271295@gmail.com>
2022-03-28 09:01:39 +0200

    push al buio che può rompere tutto

--- src/Middlewares/validation.ts
+++ src/Middlewares/validation.ts
@@ -1,7 +1,8 @@
 import { CustomValidator } from 'express-validator';
 
-export const isBoolean: CustomValidator = (param: string | undefined) => {
-	if (param !== undefined && param.toLowerCase() !== 'true' && param.toLowerCase() !== 'false')
+export const isBoolean: CustomValidator = (param: string | boolean | undefined) => {
+	console.log(param)
+	if (typeof param === 'string' && param !== undefined && param.toLowerCase() != 'true' && param.toLowerCase() != 'false')
 		throw 'Il valore deve essere true o false!';
 
 	return true;
